b57197cfbaee20bbcde5410e0be9e31514e73cf5
Adi Purnama <imadeadipurnamayasa@gmail.com>
2023-02-15 15:47:52 +0800

    USD AccountNumber

--- application/views/member/tobank/currency/USD.php
+++ application/views/member/tobank/currency/USD.php
@@ -1,3 +1,7 @@
+<div class="d-flex flex-row align-items-center my-3">
+    <input class="form-control me-2" type="text" name="accountNumber" placeholder="Account Number">
+</div>
+
 <?php if ($type == "local") { ?>
 <div class="d-flex flex-row align-items-center my-3">
     <input class="form-control me-2" type="text" name="abartn" placeholder="Routing Number">
@@ -7,10 +11,6 @@
 <?php if ($type == "inter") { ?>
 <div class="d-flex flex-row align-items-center my-3">
     <input class="form-control me-2" type="text" name="swiftCode" placeholder="Swift Code">
-</div>
-
-<div class="d-flex flex-row align-items-center my-3">
-    <input class="form-control me-2" type="text" name="accountNumber" placeholder="Account Number">
 </div>
 
 <input type="hidden" name="abartn" value="">
