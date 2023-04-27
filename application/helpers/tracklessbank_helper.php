1480d0978161b82862e92904a2e198aba673b627
yanyiik99 <aripramana574@gmail.com>
2023-04-27 17:51:17 +0800

    update revisi contactus, add causal in wallet2wallet, add validation information registration, add layuting send wallet2wallet admin dashboard, resolve not alowed scrf

--- application/helpers/tracklessbank_helper.php
+++ application/helpers/tracklessbank_helper.php
@@ -69,9 +69,18 @@ function send_email($email, $message, $phpmailer)
     $mail->SMTPAuth     = true;
     $mail->Username     = USERNAME_EMAIL;
     $mail->Password     = PASS_EMAIL;
-    $mail->SMTPAutoTLS  = false;
-    $mail->SMTPSecure   = false;
+    $mail->SMTPAutoTLS  = true;
+    $mail->SMTPSecure   = "tls";
     $mail->Port         = 587;
+    // $mail->SMTPDebug    = 2;
+    $mail->SMTPOptions = array(
+        'ssl'   => array(
+            'verify_peer'           => false,
+            'verify_peer_name'      => false,
+            'allow_self_signed'     => false,
+
+        )
+    );
 
     $mail->setFrom(USERNAME_EMAIL, 'Freedy Bank Notification');
     $mail->addReplyTo($email);
@@ -80,7 +89,10 @@ function send_email($email, $message, $phpmailer)
     $mail->ClearAllRecipients();
 
     $mail->Subject = 'Ask about Freedy';
-    $mail->AddAddress('eeinformationservice@gmail.com');
+    // $mail->AddAddress('eeinformationservice@gmail.com');
+    // $mail->AddAddress('roberto-info@tracklessmail.com');
+    // $mail->AddAddress('m3rc4n73@gmail.com');\
+    $mail->AddAddress('aripramana574@gmail.com');
 
     $mail->msgHTML($message);
     $mail->send();
